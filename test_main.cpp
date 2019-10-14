618ac3304c4523bdb439464ef97c445d981dc8f9
serik1987 <serik1987@gmail.com>
2019-10-14 10:46:58 +0300

    2D interpolation was implemented on data::ContiguousMatrix

--- test_main.cpp
+++ test_main.cpp
@@ -13,53 +13,26 @@ void test_main(){
 
     mpi::Communicator& comm = Application::getInstance().getAppCommunicator();
 
-    const int field_width = 256;
-    const double field_width_deg = 1.0;
-    const double res_x = field_width_deg / field_width;
-    const int kernel_width = 21;
-    const double kernel_width_deg = kernel_width * res_x;
+    data::ContiguousMatrix A(comm, 21, 21, 1.0, 1.0);
+    data::ContiguousMatrix Asmooth(comm, 210, 210, 1.0, 1.0);
+    data::reader::BinReader reader("matrixA.bin");
 
-    const int field_height = field_width;
-    const double field_height_deg = field_width_deg;
-    const int kernel_height = kernel_width;
-    const double kernel_height_deg = kernel_width_deg;
+    const double sigma = 0.2;
 
-    const double sigma = 0.015;
-
-    data::ContiguousMatrix A(comm, field_width, field_height, field_width_deg, field_height_deg);
-    A.fill([](auto a){
+    for (auto a = A.begin(); a != A.end(); ++a){
         double x = a.getColumnUm();
         double y = a.getRowUm();
-        if (abs(x) <= 0.25 && abs(y) <= 0.25){
-            return 1.0;
-        } else {
-            return 0.0;
-        }
-    });
+        *a = exp(-(x*x + y*y)/(2 * sigma * sigma));
+    }
+    double sum = A.sum();
+    for (auto a = A.begin(); a != A.end(); ++a){
+        *a /= sum;
+    }
     A.synchronize();
 
-    data::ContiguousMatrix K(comm, kernel_width, kernel_height, kernel_width_deg, kernel_height_deg);
-    K.fill([sigma](auto a){
-        double x = a.getColumnUm();
-        double y = a.getRowUm();
-        double value = exp(-(x*x + y*y)/(2*sigma*sigma));
-        return value;
-    });
-    double S = K.sum();
-    K.fill([S](auto a){
-        return *a/S;
-    });
-    K.synchronize();
-
-    logging::progress(0, 1, "Saving initial matrices");
-    data::reader::BinReader reader("matrixA.bin");
-    reader.save(A);
-    reader.save(K, "matrixK.bin");
-
-    logging::progress(0, 1, "Providing convolution");
-    data::LocalMatrix B(comm, field_width, field_height, field_width_deg, field_height_deg);
-    B.convolve(K, A);
-    reader.save(B, "matrixB.bin");
+    logging::progress(0, 1, "Interpolation");
+    Asmooth.interpolate(A);
+    reader.save(Asmooth);
 
     logging::progress(1, 1);
 }
